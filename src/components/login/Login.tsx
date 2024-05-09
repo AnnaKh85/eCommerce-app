0ed5e0c8c9de8710bccb7da435245e45e1ddcd5e
letmezep <letmezep@gmail.com>
2024-05-09 21:44:38 +0800

    MMG-31 feat: add password validation conditions

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,5 +1,6 @@
 import './login.css';
 
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { REGISTRATION_ROUTE } from '../../utils/constants.ts';
@@ -7,11 +8,34 @@ import Button from '../button/Button';
 import FormInput from '../input/FormInput';
 
 function LoginForm() {
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    const input = e.target as HTMLInputElement;
+
+    const checkLength = input.value.length >= 8;
+    const checkUpperCase = /[A-Z]/.test(input.value);
+    const checkLowerCase = /[a-z]/.test(input.value);
+    const checkDigit = /\d/.test(input.value);
+    const checkWhiteSpace = input.value.trim() === input.value;
+    if (input.value.length > 0) {
+      if (!checkLength || !checkUpperCase || !checkLowerCase || !checkDigit || !checkWhiteSpace) {
+        setPasswordError('enter correct password data');
+        console.log('enter correct password data');
+      } else {
+        setPasswordError('correct password data');
+        console.log('correct password data');
+      }
+    }
+  };
+
   return (
     <div className="form-container">
       <form className="login-form">
-        <FormInput placeholder="Email" className="form-input" type="text" />
-        <FormInput placeholder="Password" className="form-input" type="text" />
+        <FormInput placeholder="Password" className="form-input" type="text" value={password} onChange={handleChange} />
+        {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
         <Button
           label="Log in"
           className="button-dark"
