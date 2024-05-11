bd3043efcf61ee2606369443d387dc4911178b94
Olena Kupina <137783114+lenok-kup@users.noreply.github.com>
2024-05-11 18:48:46 +0300

    RSS-ECOMM-2_09: Validation fields in the registration form (#18)
    * RSS-ECOMM-2_09: Implement validation for required fields in the registration form
    
    * RSS-ECOMM-2_09: validate postal code based on country

---
+++ src/components/registration-form/validationSchema.ts
@@ -0,0 +1,39 @@
+import * as Yup from 'yup';
+
+export const loginFormSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 characters')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+      'Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number',
+    ),
+});
+
+export const registrationFormSchema = loginFormSchema.shape({
+  firstName: Yup.string()
+    .required('First name is required')
+    .matches(/^[a-zA-Z]+$/, 'First name must contain only letters'),
+  lastName: Yup.string()
+    .required('Last name is required')
+    .matches(/^[a-zA-Z]+$/, 'Last name must contain only letters'),
+  dob: Yup.date()
+    .required('Date of birth is required')
+    .max(new Date(Date.now() - 13 * 365 * 24 * 60 * 60 * 1000), 'You must be at least 13 years old'),
+  street: Yup.string().required('Street is required'),
+  city: Yup.string()
+    .required('City is required')
+    .matches(/^[a-zA-Z\s]*$/, 'City must contain only letters and spaces'),
+  country: Yup.string().required('Country is required'),
+  postalCode: Yup.string()
+    .required('Postal code is required')
+    .when('country', {
+      is: (value: string) => {
+        return value == 'US' || value == 'CA';
+      },
+      then: (schema) =>
+        schema.matches(/^[0-9A-Z]{6}$/, 'Postal code must contain 6 symbols, include numbers and capital letters'),
+      otherwise: (schema) => schema.matches(/^\d{5}$/, 'Postal code must contain only numbers, length of 5 symbols'),
+    }),
+});
