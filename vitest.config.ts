48a2f7760a067898d6509f478713374c28fdc4bf
Anna <anna.khizhnyakova@gmail.com>
2024-05-12 17:16:29 +0300

    MMG-84-unit-test-coverage (#21)
    * Add tests
    
    * test:
    - add vitest dependencies and setup configuration
    - add tests

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+// vite.config.ts
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
+// Vitest
+import { defineConfig as defineVitestConfig } from 'vitest/config';
+
+export default defineVitestConfig({
+  plugins: [react(), viteCommonjs(), visualizer()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts', // If you have setup files (e.g., to configure testing-library)
+  },
+});
