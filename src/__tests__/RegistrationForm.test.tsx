fc9e24861fe64a0e3a103f1b7b41f8231168b9ed
Olena Kupina <dunysha9@gmail.com>
2024-05-14 23:58:32 +0300

    disable test for registration form

--- src/__tests__/RegistrationForm.test.tsx
+++ src/__tests__/RegistrationForm.test.tsx
@@ -1,43 +1,43 @@
-// import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
-// import RegistrationForm from '../components/registration-form/RegistrationForm';
+import RegistrationForm from '../components/registration-form/RegistrationForm';
 
-// describe('RegistrationForm Submit Button', () => {
-//   it('should enable the submit button initially', async () => {
-//     render(<RegistrationForm />);
-//     const submitButton = screen.getByRole('button', { name: 'Register' });
-//     expect(submitButton).not.toBeDisabled();
-//   });
+describe.skip('RegistrationForm Submit Button', () => {
+  it('should enable the submit button initially', async () => {
+    render(<RegistrationForm />);
+    const submitButton = screen.getByRole('button', { name: 'Register' });
+    expect(submitButton).not.toBeDisabled();
+  });
 
-//   it('should disable the submit button when the form is submitting', async () => {
-//     render(<RegistrationForm />);
-//     const submitButton = screen.getByRole('button', { name: 'Register' });
-//     fireEvent.click(submitButton);
-//     expect(submitButton).toBeDisabled();
-//   });
+  it('should disable the submit button when the form is submitting', async () => {
+    render(<RegistrationForm />);
+    const submitButton = screen.getByRole('button', { name: 'Register' });
+    fireEvent.click(submitButton);
+    expect(submitButton).toBeDisabled();
+  });
 
-//   it('should re-enable the submit button after form submission is complete', async () => {
-//     render(<RegistrationForm />);
-//     const submitButton = screen.getByRole('button', { name: 'Register' });
-//     fireEvent.click(submitButton);
-//     await waitFor(() => expect(submitButton).not.toBeDisabled());
-//   });
+  it('should re-enable the submit button after form submission is complete', async () => {
+    render(<RegistrationForm />);
+    const submitButton = screen.getByRole('button', { name: 'Register' });
+    fireEvent.click(submitButton);
+    await waitFor(() => expect(submitButton).not.toBeDisabled());
+  });
 
-//   it('should disable the submit button upon form re-submission', async () => {
-//     render(<RegistrationForm />);
-//     const submitButton = screen.getByRole('button', { name: 'Register' });
-//     fireEvent.click(submitButton);
-//     await waitFor(() => expect(submitButton).not.toBeDisabled());
-//     fireEvent.click(submitButton);
-//     expect(submitButton).toBeDisabled();
-//   });
+  it('should disable the submit button upon form re-submission', async () => {
+    render(<RegistrationForm />);
+    const submitButton = screen.getByRole('button', { name: 'Register' });
+    fireEvent.click(submitButton);
+    await waitFor(() => expect(submitButton).not.toBeDisabled());
+    fireEvent.click(submitButton);
+    expect(submitButton).toBeDisabled();
+  });
 
-//   it('should enable the submit button when the form is reset after submission', async () => {
-//     render(<RegistrationForm />);
-//     const submitButton = screen.getByRole('button', { name: 'Register' });
-//     fireEvent.click(submitButton);
-//     await waitFor(() => expect(submitButton).not.toBeDisabled());
+  it('should enable the submit button when the form is reset after submission', async () => {
+    render(<RegistrationForm />);
+    const submitButton = screen.getByRole('button', { name: 'Register' });
+    fireEvent.click(submitButton);
+    await waitFor(() => expect(submitButton).not.toBeDisabled());
 
-//     expect(submitButton).not.toBeDisabled();
-//   });
-// });
+    expect(submitButton).not.toBeDisabled();
+  });
+});
