92ca38c6628d9363ed9308f4c9d68b32cdb7fe39
Anna <anna.khizhnyakova@gmail.com>
2024-05-16 08:13:55 +0300

    MMG-43: (#30)
    -Redirect users to the application's main page upon successful account creation and automatic login
    - add ProfilePage.tsx

--- src/components/login/AuthContext.tsx
+++ src/components/login/AuthContext.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import React, { createContext, useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 import setAuth from '../../utils/sendLoginData';
 import type { LoginFormValues } from './LoginForm';
@@ -7,9 +7,15 @@ import type { LoginFormValues } from './LoginForm';
 interface AuthContextType {
   isAuthenticated: boolean;
   login: (values: LoginFormValues) => void;
+  logout: () => void;
 }
 
-export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+export const AuthContext = React.createContext<AuthContextType>({
+  isAuthenticated: false,
+  login() {},
+  logout() {},
+  // username: '',
+});
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -18,19 +24,22 @@ interface AuthProviderProps {
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const login = (values: LoginFormValues) => {
-    setAuth(values);
-    console.log('user entered with values:', values);
-    setIsAuthenticated(true);
+  const login = async (values: LoginFormValues) => {
+    const response = await setAuth(values);
+    if (response.customer.email !== null || response.customer.email === '') {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+    // console.log('user entered with values:', values);
+    // setIsAuthenticated(true);
   };
 
-  //   const logout = (values: LoginFormValues) => {
-  //     setAuth(values);
-  //     console.log('user entered with values:', values);
-  //     setIsAuthenticated(false);
-  //   };
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
 
-  return <AuthContext.Provider value={{ isAuthenticated, login }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ isAuthenticated, login, logout }}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => {
---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,9 @@
+function ProfilePage() {
+  return (
+    <>
+      <h2>Profile page</h2>
+    </>
+  );
+}
+
+export default ProfilePage;
