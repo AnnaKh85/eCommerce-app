280242f7621f3ffbf90f601d18429237fec0cc17
Anna Kh <anna.khizhnyakova@gmail.com>
2024-05-19 07:16:38 +0300

    MMG-110: Responsive design

--- src/components/TopNav/TopNavResp.tsx
+++ src/components/TopNav/TopNavResp.tsx
@@ -154,7 +154,9 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar>M</Avatar>
+                <Avatar sx={{ bgcolor: '#ddb9d5' }}>
+                  <DirectionsBikeIcon sx={{ color: '#2A254B' }} />
+                </Avatar>
               </IconButton>
             </Tooltip>
             <Menu
