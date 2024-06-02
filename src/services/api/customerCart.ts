cb6caa3d7235cae09296c873c394e39991719939
Anna Kh <anna.khizhnyakova@gmail.com>
2024-06-02 18:48:38 +0300

    MMG-91: shopping cart integration - Add customerCart.ts - Add interfaces.ts

---
+++ src/services/api/customerCart.ts
@@ -0,0 +1,101 @@
+import { BASE_URL, PRODUCT_KEY } from '../constants.ts';
+import type { ICart, ICartActions, ICartPages } from '../interfaces.ts';
+
+export async function getAllCarts() {
+  const authorizationToken: string = sessionStorage.getItem('anonymousToken')!;
+  const response = await fetch(`${BASE_URL}/${PRODUCT_KEY}/me/carts`, {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + authorizationToken,
+    },
+  });
+  const carts: ICartPages = await response.json();
+  return carts;
+}
+
+export async function getMyCart(id: string) {
+  const authorizationToken: string = sessionStorage.getItem('anonymousToken')!;
+  const response = await fetch(`${BASE_URL}/${PRODUCT_KEY}/me/carts/${id}`, {
+    headers: {
+      Authorization: 'Bearer ' + authorizationToken,
+    },
+  });
+  const cart: ICart = await response.json();
+  return cart;
+}
+
+export async function getActiveCart() {
+  const authorizationToken: string = sessionStorage.getItem('anonymousToken')!;
+  const response = await fetch(`${BASE_URL}/${PRODUCT_KEY}/me/active-cart`, {
+    headers: {
+      Authorization: 'Bearer ' + authorizationToken,
+    },
+  });
+
+  const cart: ICart = await response.json();
+  return cart;
+}
+
+export async function setCountry(cartId: string, cartVersion: string) {
+  const authorizationToken: string = sessionStorage.getItem('anonymousToken')!;
+  const response = await fetch(`${BASE_URL}/${PRODUCT_KEY}/me/${cartId}`, {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer' + authorizationToken,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      version: cartVersion,
+      actions: [
+        {
+          action: 'setCountry',
+          country: 'RU',
+        },
+      ],
+    }),
+  });
+  const cart: ICart = await response.json();
+  return cart;
+}
+
+export async function updateMyCart(id: string, version: number, actions: ICartActions[]): Promise<ICart> {
+  const authorizationToken: string = sessionStorage.getItem('anonymousToken')!;
+  const response = await fetch(`${BASE_URL}/${PRODUCT_KEY}/me/carts/${id}`, {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + authorizationToken,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ version: version, actions: actions }),
+  });
+  const cart: ICart = await response.json();
+  return cart;
+}
+
+export async function clearCartById(id: string, version: number): Promise<ICart> {
+  const authorizationToken: string = sessionStorage.getItem('anonymousToken')!;
+  const response = await fetch(`${BASE_URL}/${PRODUCT_KEY}/me/carts/${id}?version=${version}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Bearer ' + authorizationToken,
+    },
+  });
+  const cart: ICart = await response.json();
+  return cart;
+}
+
+export async function createCart(): Promise<ICart> {
+  const authorizationToken: string = sessionStorage.getItem('anonymousToken')!;
+  const response = await fetch(`${BASE_URL}/${PRODUCT_KEY}/me/carts`, {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + authorizationToken,
+    },
+    body: JSON.stringify({
+      currency: 'RUB',
+      country: 'RU',
+    }),
+  });
+  const cart: ICart = await response.json();
+  return cart;
+}
