1a7976155cd5a608a929854a2913a3a709f5c6c5
letmezep <letmezep@gmail.com>
2024-06-11 19:17:42 +0800

    feat: implement a check for the existence of the results object property

--- src/components/promo-codes/promoList.ts
+++ src/components/promo-codes/promoList.ts
@@ -10,13 +10,20 @@ export default async function getPromoList() {
       method: 'GET',
     });
     const responseData = await response.json();
-    // console.log(`TEST Promo Data: ${responseData.results[0].code}`);
-    console.log(`TEST Promo Data: ${responseData.total}`);
-    // for (let i = 0; i < responseData.count; i++) {
-    //   console.log(responseData[i].results);
-    //   console.log(`i: `, i);
-    // }
-    // console.log(`Total PromoCodeList: `, responseData.total);
+
+    for (let i = 0; i < responseData.total; i++) {
+      console.log(responseData.results[i].code);
+      if (responseData.results[i].description) {
+        console.log(responseData.results[i].description['en-GB']);
+      } else {
+        console.log('no description');
+      }
+      if (responseData.results[i].name) {
+        console.log(responseData.results[i].name['en-GB']);
+      } else {
+        console.log('no name');
+      }
+    }
     return responseData;
   } catch (error) {
     console.debug('Discount Codes Error');
