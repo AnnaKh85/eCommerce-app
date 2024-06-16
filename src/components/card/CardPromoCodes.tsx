976fcff92e8266e9047b7469e80ab6da858c786b
letmezep <letmezep@gmail.com>
2024-06-16 15:23:54 +0800

    feat: implement Promocode Block on the main page

--- src/components/card/CardPromoCodes.tsx
+++ src/components/card/CardPromoCodes.tsx
@@ -24,62 +24,87 @@ function CardPromoCodes() {
     <Container
       sx={{
         display: 'flex',
-        flexDirection: 'column',
         alignItems: 'center',
         margin: '0 auto',
         width: '100%',
         gap: '20px',
         position: 'relative',
+        overflow: 'hidden',
+        flex: 1,
       }}
     >
-      {/* <Typography variant="h4" gutterBottom sx={{ fontSize: { xs: '1.5rem', sm: '2.25rem' } }}> */}
-      {/* <Typography
-        variant="h4"
-        gutterBottom
-        sx={{
-          fontSize: { xs: '1.5rem', sm: '2.25rem' },
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          backgroundColor: 'rgba(255, 255, 255, 0.7)',
-          padding: '8px',
-        }}
-      >
-        PROMOCODE: {promoData && promoData.results[0].code}
-      </Typography> */}
-
       <Box sx={{ flexGrow: 1 }}>
         <CardContent sx={{ position: 'relative' }}>
           <Card variant="outlined" style={{ border: '0' }}>
-            <CardMedia sx={{ height: 400, width: '100%', backgroundSize: 'cover' }} image={bicycle9} title="bicycle2" />
+            <CardMedia
+              sx={{ height: 500, width: '100%', backgroundSize: 'cover', backgroundPosition: 'center' }}
+              image={bicycle9}
+              title="bicycle2"
+            />
             <Typography
               variant="h4"
               gutterBottom
               sx={{
-                fontSize: { xs: '1.5rem', sm: '2.25rem' },
+                fontSize: { xs: '1rem', sm: '1.5rem' },
+                fontWeight: '500',
                 position: 'absolute',
                 top: 0,
                 left: 0,
                 backgroundColor: '#ffeb3b',
                 padding: '8px',
+                borderRadius: '4px',
+                opacity: '0.9',
               }}
             >
-              PROMOCODE: {promoData && promoData.results[0].code}
+              PROMOCODE for summer:
+            </Typography>
+
+            <Typography
+              variant="h4"
+              gutterBottom
+              sx={{
+                fontSize: { xs: '1.5rem', sm: '2.25rem' },
+                fontWeight: '800',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                backgroundColor: '#ffeb3b',
+                padding: '8px',
+                borderRadius: '4px',
+                opacity: '0.9',
+              }}
+            >
+              {promoData && promoData.results[0].code}
+            </Typography>
+
+            <Typography
+              variant="h4"
+              gutterBottom
+              sx={{
+                fontSize: { xs: '1rem', sm: '1rem' },
+                position: 'absolute',
+                bottom: 0,
+                right: 0,
+                backgroundColor: '#ffeb3b',
+                padding: '8px',
+                borderRadius: '4px',
+                opacity: '0.9',
+              }}
+            >
+              {promoData &&
+                promoData.results.map((promo, index: number) => (
+                  <div key={index} style={{ display: 'flex', alignItems: 'center', paddingLeft: '24px' }}>
+                    {promo.description && promo.description['en-GB'] ? (
+                      <p style={{ marginLeft: '0' }}>{promo.description['en-GB']}</p>
+                    ) : (
+                      <p>No description</p>
+                    )}
+                  </div>
+                ))}
             </Typography>
           </Card>
         </CardContent>
-
-        {promoData &&
-          promoData.results.map((promo, index: number) => (
-            <div key={index} style={{ display: 'flex', alignItems: 'center', paddingLeft: '24px' }}>
-              <p style={{ marginRight: '10px' }}>{promo.code}</p>
-              {promo.description && promo.description['en-GB'] ? (
-                <p style={{ marginLeft: '0' }}>{promo.description['en-GB']}</p>
-              ) : (
-                <p>No description</p>
-              )}
-            </div>
-          ))}
       </Box>
     </Container>
   );
